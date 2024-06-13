50fa38564fe5c3cdbd34d176a18bab5923199db2
Jilmar141 <jilmar141@gmail.com>
2024-06-13 19:57:21 -0400

    modal

--- src/components/add-shortcut.tsx
+++ src/components/add-shortcut.tsx
@@ -1,16 +1,53 @@
-import { Image } from "@nextui-org/react";
+import {
+  Button,
+  Image,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@nextui-org/react";
 
 function AddShortcut() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
     <div className="flex w-[100px] h-[100px] p-2 justify-center rounded cursor-pointer hover:bg-slate-400">
       <div className="flex flex-col justify-center items-center gap-2 w-full h-[90px]">
-        <Image
-          className="w-12 h-12"
-          alt="NextUI hero Image with delay"
-          src="/plus.png"
-        />
+        <Button onPress={onOpen}>
+          <Image
+            className="w-12 h-12"
+            alt="NextUI hero Image with delay"
+            src="/plus.png"
+          />
+        </Button>
         <p className="text-white text-sm">Add shortcut</p>
       </div>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Add shortcut
+              </ModalHeader>
+              <ModalBody>
+                <Input isReadOnly type="text" label="Name" variant="bordered" />
+                <Input isReadOnly type="text" label="URL" variant="bordered" />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Cancel
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Done
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
--- src/components/dropdown-icon.tsx
+++ src/components/dropdown-icon.tsx
@@ -1,22 +1,29 @@
 import {
+  Avatar,
   Dropdown,
   DropdownItem,
   DropdownMenu,
   DropdownTrigger,
 } from "@nextui-org/react";
-import { IconMoreVert } from "./icon-more-vert";
 
 function DropdownIcon() {
   return (
-    <Dropdown placement="bottom-end">
-      <DropdownTrigger>
-        <IconMoreVert className="text-white" />
-      </DropdownTrigger>
-      <DropdownMenu aria-label="Profile Actions" variant="flat">
-        <DropdownItem key="settings">Edit shortcut</DropdownItem>
-        <DropdownItem key="team_settings">Remove</DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
+    <>
+      <Dropdown placement="bottom-end">
+        <DropdownTrigger>
+          <Avatar
+            isBordered
+            as="button"
+            className="transition-transform"
+            src="/more.png"
+          />
+        </DropdownTrigger>
+        <DropdownMenu aria-label="Profile Actions" variant="flat">
+          <DropdownItem key="settings">Edit shortcut</DropdownItem>
+          <DropdownItem key="team_settings">Remove</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </>
   );
 }
 
